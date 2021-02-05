6d0018d8cf77c4cf32f6e3b8052f1782c9c57ec8
snakewriter <49989315+snakewriter@users.noreply.github.com>
2021-02-06 02:54:30 +0500

    Day 4th

--- js/main.js
+++ js/main.js
@@ -1,23 +1,62 @@
-const flowersSlider = new Swiper('.flowers-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 6,
-
-  // Navigation arrows
-  navigation: {
+$(document).ready(function () {
+  const nav = {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
-  },
+  };
+
+  const flowersSlider = new Swiper('.flowers-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 6,
+
+    // Navigation arrows
+    navigation: nav,
+    breakpoints: {
+      300: {
+        slidesPerView: 1,
+      },
+      // when window width is >= 320px    
+      320: {
+        slidesPerView: 2,
+      },
+      // when window width is >= 576px   
+      576: {
+        slidesPerView: 3,
+      },
+      // when window width is >= 640px
+      992: {
+        slidesPerView: 6,
+      }
+    },
+  });
+
+  const reviewsSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+
+    // Navigation arrows
+    navigation: nav,
+  });
 });
 
-const reviewsSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  slidesPerView: 1,
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+$('#review-1').on('click', function () {
+  $.fancybox.open([
+    {
+      src: 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts: {
+        caption: 'First caption',
+        thumb: 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src: 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts: {
+        caption: 'Second caption',
+        thumb: 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop: false
+  });
 });
